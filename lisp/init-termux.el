00011c2f112d7c3a27829dc574dd825b11e3fd33
sherylynn <352281674@qq.com>
2024-04-07 02:12:13 +0800

    try fix error in android

--- lisp/init-termux.el
+++ lisp/init-termux.el
@@ -1,4 +1,6 @@
 (when (string-equal system-type "android")
+  ;; disable because of elpa bug in android
+(setq package-check-signature nil)
   ;; Add Termux binaries to PATH environment
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
     (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
