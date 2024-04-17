e4cb53518ffe94153e6f8104d274f23555c133ff
sherylynn <352281674@qq.com>
2024-04-18 00:55:40 +0800

    fix for android

--- lisp/init-system.el
+++ lisp/init-system.el
@@ -10,12 +10,21 @@
   ;; 需要全局变量来操作
   (make-variable-buffer-local 'use-my-package-terminal)
   (set 'my-use-package-terminal "eshell")
+  ;;果然想象力才是限制，直接在启动完后，只在具体的buffer执行就行
+  (add-hook 'emacs-startup-hook 'android-insert)
+  (defun android-insert()
+    ;; 在这里添加您要执行的命令或配置
+    (when (equal (buffer-name) "*scratch*")
+      (evil-insert-state)
+      )
+    )
   ;;如果遇上了android，设置默认为insert以方便唤出键盘
-  (add-hook 'evil-initialize
-	    (lambda ()
-	      (setq evil-default-state 'insert)
-	      )
-	    )
+  ;;(add-hook 'evil-initialize
+  ;;(lambda ()
+  ;;(setq evil-default-state 'insert)
+  ;;)
+  ;;)
+
   ;; disable because of elpa bug in android
   (setq package-check-signature nil)
   ;; Add Termux binaries to PATH environment
@@ -32,11 +41,12 @@
 
 (when (string-equal system-type "gnu/linux")
   ;;说明在这里面设置会被覆盖掉
-  (add-hook 'evil-mode-hook
-	    (lambda ()
-	      (setq evil-default-state 'insert)
-	      )
-	    )
+  ;; (add-hook 'evil-mode-hook
+  ;;    (lambda ()
+  ;;     (setq evil-default-state 'insert)
+  ;;    )
+  ;; )
   )
-(setq evil-default-state 'insert)
+;;(setq evil-default-state 'insert)
+;; 和evil-collection冲突，且不方便
 (provide 'init-system)
