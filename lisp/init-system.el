9de7f8ba53a27b959a3f23ca8ee995adbf7c6f8d
sherylynn <352281674@qq.com>
2024-04-24 23:18:03 +0800

    修正了eshell中没有命令行工具的问题

--- lisp/init-system.el
+++ lisp/init-system.el
@@ -34,10 +34,9 @@
   (setq package-check-signature nil)
 
   ;; Add Termux binaries to PATH environment
-  (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
-    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
-    (setq exec-path (append exec-path (list termuxpath))))
-
+  (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
+			 (getenv "PATH")))
+  (push "/data/data/com.termux/files/usr/bin" exec-path)
 
   (use-package emacs
     ;;:hook
