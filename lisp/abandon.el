c91f94d6a14bcc9b9306c3ce34a26536da391242
sherylynn <352281674@qq.com>
2024-04-27 09:40:00 +0800

    继续调整包

--- lisp/abandon.el
+++ lisp/abandon.el
@@ -198,3 +198,38 @@
     (meow-global-mode 1)
     )
   )
+
+;;----------------org--------------------
+;;org-bars 这个包只能在图形下进行
+(when (display-graphic-p)
+  (when (eq my-use-package-org-bars t)
+    (use-package org-bars
+      :quelpa (org-bars :fetcher github :repo "tonyaldon/org-bars")
+      :config
+      (add-hook 'org-mode-hook #'org-bars-mode)
+      )
+    )
+  )
+
+;;换一个包试试, 这个包写的不好
+;;太丑了这个包,终端下效果不好，表格竖线太粗
+(when (eq my-use-package-org-visual-outline t)
+  (use-package org-visual-outline
+    :quelpa (org-visual-outline :fetcher github :repo "legalnonsense/org-visual-outline")
+    :config
+    (org-dynamic-bullets-mode)
+    (org-visual-indent-mode)
+    )
+  )
+(when (eq use-my-package-org-dynamic-bullets t)
+  (use-package org-dynamic-bullets
+    :quelpa (org-dynamic-bullets :fetcher url :url "https://github.com/legalnonsense/org-visual-outline/raw/master/org-dynamic-bullets.el")
+    :config
+    (org-dynamic-bullets-mode)
+    )
+  (use-package org-visual-indent
+    :quelpa (org-visual-indent :fetcher url :url "https://github.com/legalnonsense/org-visual-outline/raw/master/org-visual-indent.el")
+    :config
+    (org-visual-indent-mode)
+    )
+  )
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -35,39 +35,6 @@
   (evil-org-agenda-set-keys)
   )
 
-;;org-bars 这个包只能在图形下进行
-;;(when (display-graphic-p)
-;;(when (< emacs-major-version 30)
-;;(use-package org-bars
-;;:quelpa (org-bars :fetcher github :repo "tonyaldon/org-bars")
-;;:config
-;;(add-hook 'org-mode-hook #'org-bars-mode)
-;;)
-;;)
-;;)
-
-;;换一个包试试, 这个包写的不好
-;;太丑了这个包,终端下效果不好，表格竖线太粗
-;;(when (< emacs-major-version 30)
-;;(use-package org-visual-outline
-;;:quelpa (org-visual-outline :fetcher github :repo "legalnonsense/org-visual-outline")
-;;:config
-;;(org-dynamic-bullets-mode)
-;;(org-visual-indent-mode)
-;;)
-;;)
-;;(when (< emacs-major-version 30)
-;;(use-package org-dynamic-bullets
-;;:quelpa (org-dynamic-bullets :fetcher url :url "https://github.com/legalnonsense/org-visual-outline/raw/master/org-dynamic-bullets.el")
-;;:config
-;;(org-dynamic-bullets-mode)
-;;)
-;;(use-package org-visual-indent
-;;:quelpa (org-visual-indent :fetcher url :url "https://github.com/legalnonsense/org-visual-outline/raw/master/org-visual-indent.el")
-;;:config
-;;(org-visual-indent-mode)
-;;)
-;;)
 
 ;;用一个写了corfu的作者的包
 ;;可以终端下进行渲染，但是没有缩进对齐
