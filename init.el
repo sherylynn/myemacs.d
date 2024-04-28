340867d0e03f346410474202b4be326d4c1f2d93
sherylynn <352281674@qq.com>
2024-04-29 01:44:11 +0800

    改回原来的性能优化后快多了，设置对字体后表格依然是对齐的

--- init.el
+++ init.el
@@ -1,128 +1,142 @@
 ;; ~/.emacs.d/init.el
 ;;---------------GC启动优化----begin---------------
-(let (;; 加载的时候临时增大`gc-cons-threshold'以加速启动速度。
-      (gc-cons-threshold most-positive-fixnum)
-      ;; 清空避免加载远程文件的时候分析文件。
-      (file-name-handler-alist nil))
+;;(let (;; 加载的时候临时增大`gc-cons-threshold'以加速启动速度。
+;;     (gc-cons-threshold most-positive-fixnum)
+;; 清空避免加载远程文件的时候分析文件。
+;;      (file-name-handler-alist nil))
 
-  ;; Emacs配置文件内容写到下面.
+;;来自 purcell 的性能优化
+;; Adjust garbage collection threshold for early startup (see use of gcmh below)
+(setq gc-cons-threshold (* 128 1024 1024))
+;; Process performance tuning
+(setq read-process-output-max (* 4 1024 1024))
+(setq process-adaptive-read-buffering nil)
+;; General performance tuning
+(setq jit-lock-defer-time 0)
+;; Emacs配置文件内容写到下面.
 
-  ;;加载自己的方法,在 lisp 文件夹下的
-  (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-  ;;默认终端 vterm
-  ;;(setq my-use-package-terminal "eshell")
-  ;;需要全局变量
-  (defvar my-use-package-terminal "vterm")
-  ;;更灵活 treemacs
-  (defvar my-use-package-filetree "neotree")
-  ;; neotree 支持android点击触摸
-  ;;(defvar my-use-package-filetree "treemacs")
+;;加载自己的方法,在 lisp 文件夹下的
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;;默认终端 vterm
+;;(setq my-use-package-terminal "eshell")
+;;需要全局变量
+(defvar my-use-package-terminal "vterm")
+;;更灵活 treemacs
+(defvar my-use-package-filetree "neotree")
+;; neotree 支持android点击触摸
+;;(defvar my-use-package-filetree "treemacs")
 
-  ;;中文美化包
-  ;;(defvar my-use-package-cn "emacs")
-  (defvar my-use-package-cn "cnfonts")
+;;中文美化包
+;;(defvar my-use-package-cn "emacs")
+(defvar my-use-package-cn "cnfonts")
 
-  ;;主题颜色
-  (defvar my-use-theme "doom-dark")
-  ;;(defvar my-use-theme "nasy-light")
+;;主题颜色
+(defvar my-use-theme "doom-dark")
+;;(defvar my-use-theme "nasy-light")
 
-  ;;我用的 tab-bar
-  ;;发现这俩包不冲突
-  ;;但是awesome包在android下不能点击，centaur可以
-  (defvar my-use-package-tab-bar t)
-  (defvar my-use-package-awesome-tab nil)
-  (defvar my-use-package-centaur-tabs t)
+;;我用的 tab-bar
+;;发现这俩包不冲突
+;;但是awesome包在android下不能点击，centaur可以
+(defvar my-use-package-tab-bar t)
+(defvar my-use-package-awesome-tab nil)
+(defvar my-use-package-centaur-tabs t)
 
-  ;;加载一些 init-key and init command 可能用到的方法
-  (require 'init-utils)
+;;加载一些 init-key and init command 可能用到的方法
+(require 'init-utils)
 
-  ;;init-system 中会改默认终端，改变mouse
-  (require 'init-system)
+;;init-system 中会改默认终端，改变mouse
+(require 'init-system)
 
-  ;;异步获取自动更新
-  (async-shell-command-no-window "git -C ~/.emacs.d_my pull")
-  ;;完成我其他的项目
-  (async-shell-command-no-window "git -C ~/work pull")
-  (async-shell-command-no-window "git -C ~/sh pull")
-  ;;(async-shell-command-no-window "git -C ~/.doom.d/ pull")
-  ;;(async-shell-command "git -C ~/work pull")
-  ;;不管是哪个版本的，都会提示已经有命令在运行了
+;;异步获取自动更新
+(async-shell-command-no-window "git -C ~/.emacs.d_my pull")
+;;完成我其他的项目
+(async-shell-command-no-window "git -C ~/work pull")
+(async-shell-command-no-window "git -C ~/sh pull")
+;;(async-shell-command-no-window "git -C ~/.doom.d/ pull")
+;;(async-shell-command "git -C ~/work pull")
+;;不管是哪个版本的，都会提示已经有命令在运行了
 
-  ;;加快启动速度
-  ;;(setq my-init-config-timeup "normal")
-  (setq my-init-config-timeup "fast")
-  ;;(setq my-init-config-timeup "debug")
+;;加快启动速度
+;;(setq my-init-config-timeup "normal")
+(setq my-init-config-timeup "fast")
+;;(setq my-init-config-timeup "debug")
 
-  ;;加载 use-package 和源 （把设置分离出去了，因为很少动)
-  (require 'init-package)
-
-  ;;load terminal, 跟据 my-use-package-terminal 决定终端
-  (require 'init-terminal)
-
-  ;; package switch
-  (setq my-use-package-vim "evil")
-  ;;(setq my-use-package-vim "meow")
-
-  ;;leader key
-  ;;(setq my-use-package-leader "evil-leader")  ;;evil-leader 没法复合键位
-  (setq my-use-package-leader "general") ;; general 配置未完成，可以用 whichkey 提示, 发现都不能叠词，算了
-  ;;(setq my-use-package-leader "evil") ;; 发现可以直接用 evil 来配置 配置方式和 general 有点像, 少安装个包, 也和 evil-leader 一样没法使用复合键位
-
-  ;;加载 emacs 重启的插件，方便调试 emacs
-  (use-package restart-emacs
-    :config
-    ;;重启后重新打开当前窗口
-    (setq restart-emacs-restore-frames t)
-    )
-
-  (use-package saveplace
-    :ensure nil
-    :hook (after-init . save-place-mode))
+;;加载 use-package 和源 （把设置分离出去了，因为很少动)
+(require 'init-package)
 
 
-  ;;加载 evil 相关插件（把 evil 分离出去了，因为很少动)
-  (require 'init-evil)
-
-  ;;加载 org 相关插件（把 org 分离出去了，因为很少动)
-  (require 'init-org)
-
-  (unless (display-graphic-p)
-    (xterm-mouse-mode 1)
-    (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-    (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
-    )
-
-  ;;加载 program配置
-  (require 'init-program)
-
-  ;;加载 filetree
-  (require 'init-filetree)
-
-  ;;加载 UI 界面
-  (require 'init-ui)
-
-  ;;加载快捷键
-  (require 'init-key)
-  ;;加载补全配置
-  (require 'init-completion)
-
-  ;;pyim
-  (require 'init-pyim)
-
-  ;;尝试过后来又不用的代码
-  ;;(require 'abandon)
-
-  ;;关闭 emacs 的 custom 在 init 文件里瞎下蛋拉屎
-  (setq custom-file "~/.emacs.d_my/custom.el")
-  (load custom-file 'no-error 'no-message)
-
-  ;;自动保存状态重新自动打开
-  (desktop-save-mode 1)
-  (setq desktop-dirname "~/")
-  (setq desktop-base-file-name ".emacs.desktop")
-  (desktop-read "~/")
-
+;;使用 GC 优化包
+(use-package gcmh
+  :config
+  (gcmh-mode 1)
   )
+;;load terminal, 跟据 my-use-package-terminal 决定终端
+(require 'init-terminal)
+
+;; package switch
+(setq my-use-package-vim "evil")
+;;(setq my-use-package-vim "meow")
+
+;;leader key
+;;(setq my-use-package-leader "evil-leader")  ;;evil-leader 没法复合键位
+(setq my-use-package-leader "general") ;; general 配置未完成，可以用 whichkey 提示, 发现都不能叠词，算了
+;;(setq my-use-package-leader "evil") ;; 发现可以直接用 evil 来配置 配置方式和 general 有点像, 少安装个包, 也和 evil-leader 一样没法使用复合键位
+
+;;加载 emacs 重启的插件，方便调试 emacs
+(use-package restart-emacs
+  :config
+  ;;重启后重新打开当前窗口
+  (setq restart-emacs-restore-frames t)
+  )
+
+(use-package saveplace
+  :ensure nil
+  :hook (after-init . save-place-mode))
+
+
+;;加载 evil 相关插件（把 evil 分离出去了，因为很少动)
+(require 'init-evil)
+
+;;加载 org 相关插件（把 org 分离出去了，因为很少动)
+(require 'init-org)
+
+(unless (display-graphic-p)
+  (xterm-mouse-mode 1)
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
+  )
+
+;;加载 program配置
+(require 'init-program)
+
+;;加载 filetree
+(require 'init-filetree)
+
+;;加载 UI 界面
+(require 'init-ui)
+
+;;加载快捷键
+(require 'init-key)
+;;加载补全配置
+(require 'init-completion)
+
+;;pyim
+(require 'init-pyim)
+
+;;尝试过后来又不用的代码
+;;(require 'abandon)
+
+;;关闭 emacs 的 custom 在 init 文件里瞎下蛋拉屎
+(setq custom-file "~/.emacs.d_my/custom.el")
+(load custom-file 'no-error 'no-message)
+
+;;自动保存状态重新自动打开
+(desktop-save-mode 1)
+(setq desktop-dirname "~/")
+(setq desktop-base-file-name ".emacs.desktop")
+(desktop-read "~/")
+
+;; )
 
 
 ;;---------------GC启动优化----end---------------
